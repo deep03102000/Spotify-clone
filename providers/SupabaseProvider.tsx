f64213c9f5e8db6dcb55733747a6fe6d63c888f3
Deep Kumar Sadhukhan <111382756+deep03102000@users.noreply.github.com>
2025-02-18 19:59:39 +0530

    Add files via upload

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Database } from "@/database.types";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+}
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
+  // Ensure environment variables are available
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error(
+      "Missing NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_ANON_KEY environment variables."
+    );
+  }
+
+  const [supabaseClient] = useState(() =>
+    createClientComponentClient<Database>({
+      supabaseUrl,
+      supabaseKey,
+    })
+  );
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+};
+
+export default SupabaseProvider;
